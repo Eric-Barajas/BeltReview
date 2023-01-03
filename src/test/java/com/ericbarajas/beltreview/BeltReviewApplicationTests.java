aff1e60e37e3fc63e7d4ae9243b7dc25eb76f18d
Eric-Barajas <barajaseric505@gmail.com>
2023-01-02 16:27:50 -0800

    init commit

---
+++ src/test/java/com/ericbarajas/beltreview/BeltReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericbarajas.beltreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeltReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
